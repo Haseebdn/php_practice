7eaca35e4d1c5b03cdeed3bb067e015186b5d2b8
Haseeb <haseebnazir437@gmail.com>
2026-03-12 21:34:13 +0500

    if else

--- php_classes/class1.php
+++ php_classes/class1.php
@@ -12,8 +12,63 @@
 
     <?php
     echo "<h2>Hey</h2>";
+    $userName = 'hamza';
+    $hamza = 'sain';
+    $fruits = ["mango", "banana", "apple", "strawberry"];
+    $age = 18;
     ?>
-    
+
+    <!-- print variable -->
+    <h2><?php echo $userName;    ?></h2>
+
+    <!-- <h2><?php echo $$userName;    ?></h2> -->
+
+    <!-- to print array -->
+    <h2><?php print_r($fruits);    ?></h2>
+
+
+    <!-- using if else statement -->
+
+    <!-- method 1 -->
+    <?php
+    if ($age < 18) {
+    ?>
+        <h2>You are not eligible to vote</h2>
+    <?php
+    } else if ($age >= 18) {
+
+    ?>
+        <h2>You are eligible to vote</h2>
+    <?php
+    }
+    ?>
+
+    <!-- method 2 -->
+
+    <?php
+    if ($age < 18) {
+        echo "<h2> You are not eligible to vote <h2>";
+    } else if ($age >= 18) {
+        echo "<h2> You are eligible to vote <h2>";
+    }
+    ?>
+
+    <!-- method 3 -->
+
+    <?php
+    $alert = '';
+    if ($age < 18) {
+        $alert = "You are not eligible to vote";
+    } else if ($age >= 18) {
+        $alert = "You are eligible to vote";
+    }
+    ?>
+
+    <h2>
+        <?php echo $alert; ?>
+    </h2>
+
+
 </body>
 
 </html>
